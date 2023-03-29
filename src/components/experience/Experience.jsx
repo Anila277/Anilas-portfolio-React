8374e4512dacf08bc59e64667f81bfe77e1d0199
Anila Nisar <raniyasr@gmail.com>
2023-03-29 09:14:08 -0400

    cleans

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -5,12 +5,10 @@ import { BsPatchCheckFill } from 'react-icons/bs'
 const Experience = () => {
     return (
         <section id='experience'>
-            {/* <h5>What Skills I have</h5> */}
             <h2>My Skills</h2>
 
             <div className="container experience__container">
                 <div className="experience__frontend">
-                    {/* <h3>Frontend Development</h3> */}
                     <div className="experience__content">
 
                         <article className='experience__details'>
