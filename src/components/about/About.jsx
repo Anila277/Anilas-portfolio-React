581cb097e68a49834cdd4648b12351928b98b963
Anila Nisar <raniyasr@gmail.com>
2023-03-29 08:59:28 -0400

    cleans some code

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './about.css'
-// import ME from '../../assets/anila.png'
 import { FaAward } from 'react-icons/fa'
 import { BsCodeSlash } from 'react-icons/bs'
 import { VscFolderLibrary } from 'react-icons/vsc'
@@ -9,35 +8,25 @@ import { VscFolderLibrary } from 'react-icons/vsc'
 const about = () => {
     return (
         <section id='about'>
-            {/* <h5>Get to Know</h5> */}
-            <h2>About Me</h2>
+            <h2>A Little About Me</h2>
 
             <div className='container about__container'>
-                {/* <div className='about__me'> */}
-
-                {/* <div className='about__me-image'> */}
-                {/* <img src={ME} alt="About Image" /> */}
-                {/* </div> */}
-                {/* </div> */}
                 <div className="about__content">
                     <div className="about__cards">
                         <article className="about__card">
                             <FaAward className='about__icon' />
                             <h5>Experience</h5>
-                            {/* <small>5+ Years Working</small> */}
                         </article>
 
                         <article className="about__card">
                             <BsCodeSlash className='about__icon' />
                             <h5>Skills</h5>
-                            {/* <small>+</small> */}
                         </article>
 
                         <article className="about__card">
 
                             <VscFolderLibrary className='about__icon' />
                             <h5>Projects</h5>
-                            {/* <small>4+</small> */}
                         </article>
                     </div>
 
